558578cf2989b9ebf5bbc3c62afb46d363bd3579
VictorRjs4 <143755263+VictorRjs4@users.noreply.github.com>
2024-05-23 13:27:50 -0500

    modificacion login.component.ts

--- src/app/sistemas/componets/login/login.component.ts
+++ src/app/sistemas/componets/login/login.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']  // Corregido a 'styleUrls' y usando un array
 })
 export class LoginComponent {
+  email: string = 'test@example.com';
+  password: string = '12345';
+  message: string = '';  // Añadir una nueva propiedad para el mensaje
 
+  constructor() { }
+
+  login() {
+    console.log(`Intento de inicio de sesión con email: ${this.email} y contraseña: ${this.password}`);
+    // Simular un inicio de sesión exitoso
+    this.message = "Inicio de sesión exitoso";
+  }
 }

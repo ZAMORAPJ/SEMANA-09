f720f4d3d1e60c68edd3407228ea05f4dd17f3e6
VictorRjs4 <143755263+VictorRjs4@users.noreply.github.com>
2024-05-23 13:29:25 -0500

    test de  hierba mala  app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hierbaMala');
+    expect(compiled.querySelector('h1')?.textContent).toContain('hierbaMala');
   });
 });
